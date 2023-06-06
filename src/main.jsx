5f51ed5963eb2e05207716c6c3237d66d97de743
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-06-07 05:21:53 +0600

    navbar and footer added

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { router } from "./Routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div data-theme="light">
+    <div className="max-w-screen-xl mx-auto" data-theme="light">
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>
