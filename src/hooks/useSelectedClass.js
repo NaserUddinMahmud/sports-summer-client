85fbd5e37dd7731856e877ffd94a0f7f11913ec3
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-06-11 18:47:55 +0600

    Axios secure added

--- src/hooks/useSelectedClass.js
+++ src/hooks/useSelectedClass.js
@@ -1,14 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react'
 import { AuthContext } from '../context/AuthProvider'
+import useAxiosSecure from './useAxiosSecure'
 
 const useSelectedClass = () => {
     const {user} = useContext(AuthContext)
+    // const token = localStorage.getItem('access-token')
+    const axiosSecure = useAxiosSecure()
     const {  data: selectedClasses =[],  refetch } = useQuery({
         queryKey: ['selectedClass' ],
         queryFn: async () =>{
-            const res =await fetch(`http://localhost:5000/selectedClasses?email=${user?.email}`)
-            return res.json();
+            const res =await axiosSecure(`/selectedClasses?email=${user?.email}`)
+            console.log('axios res', res);
+            return res.data;
         },
       })
       return [selectedClasses,refetch]
